47e1f05ea3097f58b5e3821245a75356a010851d
joaovictor3g <jvdias79797@gmail.com>
2022-01-16 01:57:00 -0300

    fix: adjusted utterance comments component

--- src/components/Utterance/index.tsx
+++ src/components/Utterance/index.tsx
@@ -1,21 +1,21 @@
-import styles from './styles.module.scss';
+import { useEffect, useRef } from 'react';
 
-export const UtterancesComments: React.FC = () => (
-  <section
-    className={styles.section}
-    ref={elem => {
-      if (!elem) {
-        return;
-      }
-      const scriptElem = document.createElement('script');
-      scriptElem.src = 'https://utteranc.es/client.js';
-      scriptElem.async = true;
-      scriptElem.crossOrigin = 'anonymous';
-      scriptElem.setAttribute('repo', 'joaovictor3g/blog-ignite');
-      scriptElem.setAttribute('issue-term', 'pathname');
-      scriptElem.setAttribute('label', 'blog-comment');
-      scriptElem.setAttribute('theme', 'dark-blue');
-      elem.appendChild(scriptElem);
-    }}
-  />
-);
+export function UtterancesComments() {
+  const utteranceRef = useRef<HTMLDivElement>();
+
+  useEffect(() => {
+    if (!utteranceRef.current) return;
+
+    const scriptElem = document.createElement('script');
+    scriptElem.src = 'https://utteranc.es/client.js';
+    scriptElem.async = true;
+    scriptElem.crossOrigin = 'anonymous';
+    scriptElem.setAttribute('repo', 'joaovictor3g/blog-ignite');
+    scriptElem.setAttribute('issue-term', 'pathname');
+    scriptElem.setAttribute('label', 'blog-comment');
+    scriptElem.setAttribute('theme', 'dark-blue');
+    utteranceRef.current.appendChild(scriptElem);
+  }, []);
+
+  return <div ref={utteranceRef}></div>;
+}
